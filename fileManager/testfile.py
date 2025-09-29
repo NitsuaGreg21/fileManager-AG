18f83a1547dcce80f829ff9ddc6e34f2467d0a00
Austin Gregory <eraser.cat21@gmail.com>
2025-09-29 13:18:52 -0500

    Delete fileManager/testfile.py
    testfile.py wasn't a necessity to the fileManager program.

--- fileManager/testfile.py
+++
@@ -1,9 +0,0 @@
-def fileStart():
-    userQuestion = input("Would you like to read a file, edit a file, or create a new file? (r/e/c): ").lower()
-    fileDirectory = "TextFiles"
-    txtFileExtension = ".txt"
-    fileName = input("Could you give me a file name?: ")
-    file = fileDirectory + fileName + txtFileExtension 
-    print(file)
-
-fileStart()
